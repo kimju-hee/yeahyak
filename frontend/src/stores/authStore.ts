ff7716d2f95cbb32422580490c58c58e7dbfd0eb
s0nghyunje0ng <oct-2@naver.com>
2025-07-29 21:58:54 +0900

    fe/feat: 로그인 시 스토어에 프로필 정보 저장

--- frontend/src/stores/authStore.ts
+++ frontend/src/stores/authStore.ts
@@ -1,12 +1,13 @@
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
-import { mockUsers } from '../mocks/auth.mock';
-import type { User } from '../mocks/types';
+import { mockAdmins, mockPharmacies, mockUsers } from '../mocks/auth.mock';
+import type { Admin, Pharmacy, User } from '../mocks/types';
 
 interface AuthState {
   user: User | null;
   token: string | null;
   isAuthenticated: boolean;
+  profile: Admin | Pharmacy | null;
 
   login: (email: string, password: string) => Promise<void>;
   logout: () => void;
@@ -18,16 +19,24 @@ export const useAuthStore = create<AuthState>()(
       user: null,
       token: null,
       isAuthenticated: false,
+      profile: null,
 
       login: async (email, password) => {
         const foundUser = mockUsers.find(
           (user) => user.email === email && user.password === password,
         );
         if (foundUser) {
+          let profile = null;
+          if (foundUser.role === 'ADMIN') {
+            profile = mockAdmins.find((admin) => admin.userId === foundUser.id) || null;
+          } else if (foundUser.role === 'BRANCH') {
+            profile = mockPharmacies.find((pharmacy) => pharmacy.userId === foundUser.id) || null;
+          }
           set({
             user: foundUser,
             token: `token-${foundUser.id}`, // 임의의 토큰 생성
             isAuthenticated: true,
+            profile: profile,
           });
         } else {
           throw new Error('이메일 또는 비밀번호가 올바르지 않습니다.');
@@ -39,6 +48,7 @@ export const useAuthStore = create<AuthState>()(
           user: null,
           token: null,
           isAuthenticated: false,
+          profile: null,
         });
       },
     }),
