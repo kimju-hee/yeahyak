8f862f2a977f5055a16ffc842231d67e2a5dde89
Amilaje <choijinho321@gmail.com>
2025-08-05 14:27:54 +0900

    be/fix : Fix the Product Announcement API

--- backend/src/main/java/com/yeahyak/backend/dto/ProductRequestDTO.java
+++ backend/src/main/java/com/yeahyak/backend/dto/ProductRequestDTO.java
@@ -11,7 +11,6 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Data
 public class ProductRequestDTO {
     private String productName;
     private String productCode;
