dc1c241b0b08a5a6063b810ae14df00f23f63926
Amilaje <choijinho321@gmail.com>
2025-08-19 17:26:33 +0900

    be/fix : 예측한 모델 product code기반 조회 기능 구현으로 구조 리모델링

---
+++ backend/src/main/java/com/yeahyak/backend/exception/FlaskPredictException.java
@@ -0,0 +1,8 @@
+package com.yeahyak.backend.exception;
+
+
+public class FlaskPredictException extends RuntimeException {
+    public FlaskPredictException(String message) {
+        super(message);
+    }
+}
