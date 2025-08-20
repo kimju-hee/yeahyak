6664c5bb52c47edb5d68fc6a66e9024b5ee6ab70
Juhee <101857307+kimju-hee@users.noreply.github.com>
2025-08-20 17:19:41 +0900

    be/fix: 공지사항 조회 문제 해결 (#156)

--- backend/src/main/java/com/yeahyak/backend/repository/AnnouncementRepository.java
+++ backend/src/main/java/com/yeahyak/backend/repository/AnnouncementRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
     Page<Announcement> findByType(AnnouncementType type, Pageable pageable);
+
     Page<Announcement> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(
             String titleKeyword, String contentKeyword, Pageable pageable
     );
