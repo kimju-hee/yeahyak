a5a36bd5157d19ad0d413cc9e3284fc665e20b89
s0nghyunje0ng <oct-2@naver.com>
2025-08-21 05:54:11 +0900

    be/fix: 공지사항 키워드 필터링 문제 해결

--- backend/src/main/java/com/yeahyak/backend/controller/AnnouncementController.java
+++ backend/src/main/java/com/yeahyak/backend/controller/AnnouncementController.java
@@ -1,22 +1,34 @@
 package com.yeahyak.backend.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.yeahyak.backend.dto.AnnouncementRequestDTO;
 import com.yeahyak.backend.dto.JinhoResponse;
 import com.yeahyak.backend.entity.Announcement;
 import com.yeahyak.backend.service.AnnouncementService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/announcements")
@@ -67,9 +79,11 @@ public class AnnouncementController {
     ) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "createdAt"));
 
-        Page<Announcement> pagedResult;
 
-        if (keyword != null && !keyword.isEmpty()) {
+        Page<Announcement> pagedResult;
+        if (keyword != null && !keyword.isEmpty() && type != null && !type.isEmpty()) {
+            pagedResult = announcementService.searchAnnouncementsWithType(type, keyword, pageable);
+        } else if (keyword != null && !keyword.isEmpty()) {
             pagedResult = announcementService.searchAnnouncements(keyword, pageable);
         } else {
             pagedResult = announcementService.findAllPaged(page, size, type);
