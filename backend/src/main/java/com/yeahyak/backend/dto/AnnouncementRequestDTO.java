912df4544145e5f0bef31aaf4fdaa4cfdc8ab3ba
Juhee <101857307+kimju-hee@users.noreply.github.com>
2025-08-20 13:39:02 +0900

    be/fix: 공지사항 부분 파일 업로드 POST, GET 로직 작성 (#155)
    - azure blob 연동

--- backend/src/main/java/com/yeahyak/backend/dto/AnnouncementRequestDTO.java
+++ backend/src/main/java/com/yeahyak/backend/dto/AnnouncementRequestDTO.java
@@ -2,11 +2,12 @@ package com.yeahyak.backend.dto;
 
 import com.yeahyak.backend.entity.enums.AnnouncementType;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class AnnouncementRequestDTO {
     private AnnouncementType type;
     private String title;
     private String content;
-    private String attachmentUrl;
 }
