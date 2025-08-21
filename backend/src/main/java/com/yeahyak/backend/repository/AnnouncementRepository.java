2513b00e21e4d277b445819777cc8107a27185b9
s0nghyunje0ng <oct-2@naver.com>
2025-08-21 11:26:55 +0900

    be/fix: 공지사항 키워드 필터링 시 타입 무시되는 문제 해결 (#162)

--- backend/src/main/java/com/yeahyak/backend/repository/AnnouncementRepository.java
+++ backend/src/main/java/com/yeahyak/backend/repository/AnnouncementRepository.java
@@ -1,16 +1,21 @@
 package com.yeahyak.backend.repository;
 
-import com.yeahyak.backend.entity.Announcement;
-import com.yeahyak.backend.entity.enums.AnnouncementType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yeahyak.backend.entity.Announcement;
+import com.yeahyak.backend.entity.enums.AnnouncementType;
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long> {
     Page<Announcement> findByType(AnnouncementType type, Pageable pageable);
 
     Page<Announcement> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(
-            String titleKeyword, String contentKeyword, Pageable pageable
+        String titleKeyword, String contentKeyword, Pageable pageable
+    );
+
+    Page<Announcement> findByTypeAndTitleContainingIgnoreCaseOrTypeAndContentContainingIgnoreCase(
+        AnnouncementType type1, String titleKeyword, AnnouncementType type2, String contentKeyword, Pageable pageable
     );
 }
 
