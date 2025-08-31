a24227f82bf8d4f5bbd18cb4b65d7098ff67f58f
s0nghyunje0ng <oct-2@naver.com>
2025-09-01 03:32:11 +0900

    fe/refactor: 에러 페이지 디자인 수정

--- frontend/src/pages/Common/Error/ForbiddenPage.tsx
+++ frontend/src/pages/Common/Error/ForbiddenPage.tsx
@@ -1,11 +1,26 @@
-import { Result } from 'antd';
+import { Button, Result, Typography } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 export default function ForbiddenPage() {
+  const navigate = useNavigate();
   return (
     <Result
       status="403"
-      title="403"
-      subTitle="Sorry, you are not authorized to access this page."
+      title={
+        <Typography.Title level={1} style={{ color: 'white' }}>
+          403
+        </Typography.Title>
+      }
+      subTitle={
+        <Typography.Title level={4} style={{ color: 'white' }}>
+          접근 권한이 없습니다.
+        </Typography.Title>
+      }
+      extra={
+        <Button ghost onClick={() => navigate('/')}>
+          돌아가기
+        </Button>
+      }
     />
   );
 }
--- frontend/src/pages/Common/Error/NotFoundPage.tsx
+++ frontend/src/pages/Common/Error/NotFoundPage.tsx
@@ -1,5 +1,26 @@
-import { Result } from 'antd';
+import { Button, Result, Typography } from 'antd';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFoundPage() {
-  return <Result status="404" title="404" subTitle="Sorry, the page you visited does not exist." />;
+  const navigate = useNavigate();
+  return (
+    <Result
+      status="404"
+      title={
+        <Typography.Title level={1} style={{ color: 'white' }}>
+          404
+        </Typography.Title>
+      }
+      subTitle={
+        <Typography.Title level={4} style={{ color: 'white' }}>
+          찾을 수 없는 페이지입니다.
+        </Typography.Title>
+      }
+      extra={
+        <Button ghost onClick={() => navigate('/')}>
+          돌아가기
+        </Button>
+      }
+    />
+  );
 }
