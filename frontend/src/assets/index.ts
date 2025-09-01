95f72fb38012ec7618aa6f008a292babf9c40c3c
s0nghyunje0ng <oct-2@naver.com>
2025-09-01 23:00:10 +0900

    fe/feat: 랜딩 페이지 정적 파일 추가

--- frontend/src/assets/index.ts
+++ frontend/src/assets/index.ts
@@ -1,19 +1,27 @@
-import background01 from './background01.jpg';
-import background02 from './background02.jpg';
-import background03 from './background03.jpg';
-import landing01 from './landing01.jpg';
-import landing02 from './landing02.jpg';
-import landing03 from './landing03.jpg';
-import landing04 from './landing04.jpg';
+import chatbot3d from './chatbot_3d.png';
+import chatbottext from './chatbot_text.png';
+import forecasttext from './forecast_text.png';
+import forecastbot3d from './forecastbot_3d.png';
+import inventorytext from './inventory_text.png';
+import inventorybot3d from './inventorybot_3d.png';
+import landingbg from './landing_bg.jpg';
 import logo from './logo.png';
+import noticetext from './notice_text.png';
+import noticebot3d from './noticebot_3d.png';
+import pill3d from './pill_3d.png';
+import text from './text.png';
 
 export {
-  background01,
-  background02,
-  background03,
-  landing01,
-  landing02,
-  landing03,
-  landing04,
+  chatbot3d,
+  chatbottext,
+  forecastbot3d,
+  forecasttext,
+  inventorybot3d,
+  inventorytext,
+  landingbg,
   logo,
+  noticebot3d,
+  noticetext,
+  pill3d,
+  text,
 };
